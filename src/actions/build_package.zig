6f4f898e558d57b9bca63f78554fb77983eb4f9e
D-Berg <danielfjberg@gmail.com>
2025-12-25 11:50:41 +0100

    lua: can modify zig env

--- src/actions/build_package.zig
+++ src/actions/build_package.zig
@@ -31,7 +31,16 @@ pub fn build(io: Io, gpa: Allocator, env: *std.process.EnvMap, args: BuildArgs) 
 
     lua.requiref("_G", zlua.Lib.base, true);
 
-    try lua.loadString(manifest);
+    lua.setGlobal("load");
+    lua.pushNil();
+    lua.setGlobal("loadfile");
+    lua.pushNil();
+    lua.setGlobal("dofile");
+
+    var lua_script_name_buf: [128]u8 = undefined;
+    const lua_script_name = try std.fmt.bufPrintZ(&lua_script_name_buf, "@{s}.lua", .{args.package_name});
+
+    try lua.loadBuffer(manifest, lua_script_name);
     try lua.pcallk(0, 1, 0, 0, null);
 
     const manifest_idx = lua.getTop();
@@ -69,6 +78,47 @@ pub fn build(io: Io, gpa: Allocator, env: *std.process.EnvMap, args: BuildArgs) 
     _ = lua.pushlString(try std.fmt.bufPrint(&print_buf, "{t}", .{builtin.cpu.arch}));
     lua.setField(ctx, "arch");
 
+    lua.newTable();
+    const env_table = lua.getTop();
+    lua.pushCFunction(luaEnvSet);
+    lua.setField(env_table, "set");
+
+    lua.pushLightUserdata(@ptrCast(@alignCast(env)));
+    lua.setField(env_table, "ud");
+
+    lua.setField(ctx, "env");
+
     // build(ctx)
     try lua.pcallk(1, 0, 0, 0, null);
+}
+
+fn luaEnvSet(state: ?*zlua.LuaState) callconv(.c) c_int {
+    std.debug.print("lua called: lua_env_set\n", .{});
+    const lua: zlua.State = .{ .inner = state.? };
+
+    var print_buf: [128]u8 = undefined;
+
+    _ = lua.getField(1, "ud");
+
+    const ud = lua.toUserdata(-1) orelse {
+        lua.pushBoolean(false);
+        _ = lua.pushlString("null userdata");
+        return 2;
+    };
+    lua.pop(1); // restore stack
+    const env_map: *std.process.EnvMap = @ptrCast(@alignCast(ud));
+
+    const key = lua.toLString(2);
+    const value = lua.toLString(3);
+
+    env_map.put(key, value) catch |err| {
+        const err_str = std.fmt.bufPrint(&print_buf, "{t}", .{err}) catch &.{};
+
+        lua.pushBoolean(false);
+        _ = lua.pushlString(err_str);
+        return 2;
+    };
+
+    lua.pushBoolean(true);
+    return 1;
 }
